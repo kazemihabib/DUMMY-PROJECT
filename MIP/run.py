a3186042f841784a6da87173ec96ce5549c39d39
unknown <safoura.banihashemi3@gmail.com>
2024-11-25 22:39:35 +0100

    fixing

--- MIP/run.py
+++ MIP/run.py
@@ -15,11 +15,38 @@ import argparse
 
 TIME_LIMIT = 300
 
-def solve_model(input_dir, output_dir, instance, time_limit = TIME_LIMIT):
+def solve_model(output_dir, model, instance, solver_name, solver, solution_data, route_decision_vars, num_couriers, num_items, time_limit = TIME_LIMIT):
     """
     Solves the given optimization model with the specified solver and places the results in a queue.
     """
+   
+    try :
+        
+        model.solve(solver)
+        status = model.status
+        if status == 1:  # Feasible solution
+            solve_time = min(time_limit, floor(model.solutionTime))
+            is_optimal = solve_time < time_limit
+            solution_data[solver_name] = create_solution_json(
+                route_decision_vars, num_items + 1, num_couriers, solve_time, is_optimal, value(model.objective)
+            )
+        else:
+            solution_data[solver_name] = create_solution_json(None, 0, 0, time_limit, False, -1)
+    except:
+        solution_data[solver_name] = create_solution_json(None, 0, 0, time_limit, False, -1)
+        
+        save_solution_as_json(instance,solution_data, output_dir)
 
+
+
+
+def run(input_dir, output_dir, instance=0, time_limit=TIME_LIMIT):
+    """
+    Executes the optimization process for one or multiple instances using various solvers
+    with forced termination if solving exceeds the time limit.
+    """
+
+    
     solvers = {
         "CBC": PULP_CBC_CMD(timeLimit=time_limit),
         "HiGHS": getSolver('HiGHS', timeLimit=time_limit, msg=False),
@@ -39,40 +66,18 @@ def solve_model(input_dir, output_dir, instance, time_limit = TIME_LIMIT):
         model, route_decision_vars, courier_distances = setup_model(
             num_couriers, num_items, courier_capacity, item_sizes, distance_matrix
         )
+
+        solution_data = {}
         
-        try :
-            solution_data = {}
-            for solver_name, solver in solvers.items():
-                model.solve(solver)
-                status = model.status
-                if status == 1:  # Feasible solution
-                    solve_time = min(time_limit, floor(model.solutionTime))
-                    is_optimal = solve_time < time_limit
-                    solution_data[solver_name] = create_solution_json(
-                        route_decision_vars, num_items + 1, num_couriers, solve_time, is_optimal, value(model.objective)
-                    )
-                else:
-                    solution_data[solver_name] = create_solution_json(None, 0, 0, time_limit, False, -1)
-        except:
-            solution_data[solver_name] = create_solution_json(None, 0, 0, time_limit, False, -1)
-        
-        save_solution_as_json(instance, solution_data, output_dir) # Save results
+    for solver_name, solver in solvers.items():
+        process = multiprocessing.Process(target=solve_model, args=(output_dir, model, instance, solver_name, solver, solution_data, route_decision_vars, num_couriers, num_items))
+        process.start()  # Start the solver in a separate process
+        process.join(timeout = time_limit)  # Wait for the process to complete or timeout
 
-
-def run(input_dir, output_dir,instance=0, time_limit=TIME_LIMIT):
-    """
-    Executes the optimization process for one or multiple instances using various solvers
-    with forced termination if solving exceeds the time limit.
-    """
-    
-    process = multiprocessing.Process(target=solve_model, args=(input_dir, output_dir, instance, time_limit))
-    process.start()  # Start the solver in a separate process
-    process.join(timeout = time_limit)  # Wait for the process to complete or timeout
-
-    if process.is_alive():
-        # Solver exceeded the time limit
-        process.terminate()  # Force terminate the solver
-        process.join()  # Ensure the process is cleaned up
+        if process.is_alive():
+            # Solver exceeded the time limit
+            process.terminate()  # Force terminate the solver
+            process.join()  # Ensure the process is cleaned up
 
 
 if __name__=="__main__":
